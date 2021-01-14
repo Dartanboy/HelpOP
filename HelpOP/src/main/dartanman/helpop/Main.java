b272add8762bf3dd2ccf89d8382b00907a278369
Dartanman <dartanmanplaysminecraft@gmail.com>
2021-01-14 12:00:51 -0600

    Update comments
    I didn't include documentation, so I added it in.

--- HelpOP/src/main/dartanman/helpop/Main.java
+++ HelpOP/src/main/dartanman/helpop/Main.java
@@ -1,3 +1,8 @@
+/**
+* JavaPlugin class for the entire HelpOP plugin.
+* @author Dartanman (Austin Dart)
+**/
+
 package main.dartanman.helpop;
 
 import org.bukkit.plugin.java.JavaPlugin;
@@ -6,12 +11,18 @@ import main.dartanman.helpop.commands.HelpOPCmd;
 
 public class Main extends JavaPlugin{
 	
+	/**
+	* Enables the plugin by creating the configuration file and registering the /helpop command.
+	**/
 	public void onEnable() {
 		getConfig().options().copyDefaults(true);
 		saveConfig();
 		getCommand("helpop").setExecutor(new HelpOPCmd(this));
 	}
 	
+	/**
+	* Would disable the plugin, but nothing has to happen to disable HelpOP. The server handles it automatically.
+	**/
 	public void onDisable() {
 		
 	}
